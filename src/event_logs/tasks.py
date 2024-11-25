8f7dee2a46107d1b4b78b10dca5703bc4e83fb0b
Valentin Vikhorev <vvihorevv@gmail.com>
2024-11-26 00:55:06 +0100

    Use EventLogClient as a contextmanager in celery task

--- src/event_logs/tasks.py
+++ src/event_logs/tasks.py
@@ -1,30 +1,12 @@
-import clickhouse_connect
 import structlog
 from celery import shared_task
-from django.conf import settings
-from sentry_sdk import capture_exception
 
-from core.event_log_client import pull_logged_events
+from core.event_log_client import EventLogClient
 
 logger = structlog.get_logger(__name__)
 
 
 @shared_task
-def pull_outbox_events_log() -> None:
-    client = clickhouse_connect.get_client(
-        host=settings.CLICKHOUSE_HOST,
-        port=settings.CLICKHOUSE_PORT,
-        user=settings.CLICKHOUSE_USER,
-        password=settings.CLICKHOUSE_PASSWORD,
-        query_retries=2,
-        connect_timeout=30,
-        send_receive_timeout=10,
-    )
-
-    try:
-        pull_logged_events(client)
-    except Exception as e:
-        capture_exception(e)
-        logger.error("error pushing event log outbox to clickhouse", error=str(e))
-    finally:
-        client.close()
+def pull_and_publish_outbox_events_log() -> None:
+    with EventLogClient.init() as client:
+        client.pull_and_publish_logged_events()
